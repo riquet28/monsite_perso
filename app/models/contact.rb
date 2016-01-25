6897cdd802d47638da84183ceb39d99c8c8c9072
riquet <eric.merlin3@gmail.com>
2016-01-25 01:02:07 +0100

    Réglage envoi de mail

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,13 +3,13 @@ class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  attribute :nickname,  :captcha  => true
+  attribute :telephone
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
+      :subject => "Formulaire de contact site perso",
       :to => "eric.merlin3@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
